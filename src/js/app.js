c6a44c88ecd11f672e23169436e780f1723da537
jcrews123 <168486450+jcrews123@users.noreply.github.com>
2024-06-13 00:17:45 +0000

    Fixed the social media bar

--- src/js/app.js
+++ src/js/app.js
@@ -38,7 +38,7 @@ function render(variables = {}) {
           <h3>${variables.city === null ? "City" : variables.city}, ${
     variables.country === null ? "Country" : variables.country
   }</h3>
-          <ul class="position-right">
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/${
               variables.twitter
             }"><i class="fab fa-twitter"></i></a></li>
